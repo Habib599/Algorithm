6556f33f469abfb161f0d51d59121ff1ed02ba6a
Md Habibur Rahman <mdhabiburr599@gmail.com>
2023-08-12 22:10:33 +0600

    hi

--- module 8 Assignment/component node count.cpp
+++ module 8 Assignment/component node count.cpp
@@ -1,42 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int N = 1e3+5;
+
+const int N = 1e3 + 5;
 vector<int> adj[N];
 bool visited[N];
 
-int bfs(int s)
-{
-    queue<int> q;
-    q.push(s);
+int dfs(int s) {
     visited[s] = true;
-    int node = 0;
+    int node = 1;  // Counting the current node
 
-    while(!q.empty())
-    {
-        int x = q.front();
-        q.pop();
-        node++;
-
-        for(int v : adj[x])
-        {
-            if(!visited[v])
-            {
-                q.push(v);
-                visited[v] = true;
-            }
+    for (int v : adj[s]) {
+        if (!visited[v]) {
+            node += dfs(v);
         }
     }
 
     return node;
 }
 
-int main()
-{
+int main() {
     int n, m;
     cin >> n >> m;
 
-    for(int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
         adj[a].push_back(b);
@@ -45,13 +31,10 @@ int main()
 
     vector<int> v;
 
-    for(int i = 0; i < N; i++)
-    {
-        if(!visited[i])
-        {
-            int cnode = bfs(i);
-            if(cnode > 1)
-            {
+    for (int i = 0; i < N; i++) {
+        if (!visited[i]) {
+            int cnode = dfs(i);
+            if (cnode > 1) {
                 v.push_back(cnode);
             }
         }
@@ -59,8 +42,7 @@ int main()
 
     sort(v.begin(), v.end());
 
-    for(int cnode : v)
-    {
+    for (int cnode : v) {
         cout << cnode << " ";
     }
 

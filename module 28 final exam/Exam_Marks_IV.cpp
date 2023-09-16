3ee33463343c9792ff5a74592a0e18909a890811
Md Habibur Rahman <mdhabiburr599@gmail.com>
2023-09-16 21:02:01 +0600

    hi
    hi

--- module 28 final exam/Exam_Marks_IV.cpp
+++ module 28 final exam/Exam_Marks_IV.cpp
@@ -1,42 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
+const long long MOD=1e9+7;
 
 int main() 
 {
     int T;
     cin >> T;
-    
+
     while (T--) 
     {
         int N, M;
         cin >> N >> M;
 
         int marks[N];
+
         for (int i = 0; i < N; i++) 
         {
             cin >> marks[i];
         }
 
-        int dp[1001];
-        fill(dp, dp + 1001, 1001);
-        dp[0] = 0;
+        
+        vector<long long> dp(1001, 0);
+        dp[0] = 1;
 
         for (int i = 0; i < N; i++) 
         {
             for (int j = marks[i]; j <= 1000; j++) 
             {
-                dp[j] = min(dp[j], dp[j - marks[i]] + 1);
+                dp[j] = (dp[j] + dp[j - marks[i]]) % MOD;
             }
         }
-
-        if (dp[1000 - M] == 1001) 
-        {
-            cout << "-1" << endl;  
-        } 
-        else 
-        {
-            cout << dp[1000 - M] << endl;
-        }
+        cout << dp[1000 - M] << endl;
     }
 
     return 0;
